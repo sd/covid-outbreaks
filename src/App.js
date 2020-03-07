11e9a390575d66e4b4dcd26403d4d5741c5e9904
Sebastian Delmont <sd@notso.net>
2020-03-07 07:18:50 -0500

    COVID-19 Outbreak Visualizer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>COVID-19 Outbreaks</h1>
       </header>
+      <footer className="App-footer">
+        By Sebastián Delmont
+        {' • '}
+        <a href='https://twitter.com/sd'>@sd</a>
+      </footer>
     </div>
   );
 }
