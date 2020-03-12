1261ae6b28eb6335616a60e0718a59160001563d
Sebastian Delmont <sd@notso.net>
2020-03-12 08:58:02 -0400

    prelim

--- src/data/preliminaryData.js
+++ src/data/preliminaryData.js
@@ -2,25 +2,17 @@ export const PRELIMINARY_DATA = {
   total: {
     deaths:  {
       '3/12/20': {
-
         // 'Italy': 827,
-        // 'Iran': 354,
-        // 'Spain': 54,
+        'Iran': 429,
+        'South Korea': 66,
+        'Spain': 64,
+        'Japan': 16,
+        'Switzerland': 6,
         // 'France': 48,
-        // 'South Korea': 60,
         // 'United Kingdom': 8,
         // 'Germany': 3,
-        // 'Switzerland': 4,
-        // 'Netherlands': 5,
-        // 'Belgium': 3,
-        // 'Lebanon': 2,
-        // 'Indonesia': 1,
-        // 'Albania': 1,
-        // 'Panama': 1,
-        // 'USA > South Dakota': 1,
-        // 'Japan': 15,
-        // 'Sweden': 1,
         // 'USA > Washington': 24,
+        'Austria': 1,
       }
     }
   },
