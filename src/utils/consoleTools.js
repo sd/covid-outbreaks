a2f91b320f95160d582db2e2467dc4ce2ff515f1
Sebastian Delmont <sd@notso.net>
2020-03-28 16:58:08 -0400

    console

--- src/utils/consoleTools.js
+++ src/utils/consoleTools.js
@@ -20,7 +20,7 @@ export function setupConsoleTools (data, dates, dispatch) {
 export function covidProjection(country, options = {}) {
   const entry = window.covid.entries[country]
 
-  // let dayCount = options.days || 7
+  let dayCount = options.days || 14
 
   let last7days = window.covid.dates.slice(-7)
   if (!entry.totals.deaths[last7days[6]]) {
@@ -30,7 +30,7 @@ export function covidProjection(country, options = {}) {
   let total = entry.totals.deaths[last7days[6]]
   let velocity = entry.velocity.deaths[last7days[6]]
   let acceleration = options.acc || entry.rollingAcceleration.deaths[last7days[6]]
-  let accelerationDelta = options.accDelta || entry.rollingAcceleration.deaths[last7days[6]] - entry.rollingAcceleration.deaths[last7days[5]]
+  let accelerationDelta = options.delta || entry.rollingAcceleration.deaths[last7days[6]] - entry.rollingAcceleration.deaths[last7days[5]]
 
   console.log(`%c${entry.emoji} ${entry.code} • ${entry.name}`, 'font-weight: bold')
   console.log(
@@ -40,36 +40,56 @@ export function covidProjection(country, options = {}) {
     + `    acc: ${padStart(numeral(acceleration).format('0.000'), 6)}`,
     'color: #000'
   )
-  console.log(`        assuming accDelta of ${numeral(accelerationDelta).format('0.000')} per day`)
+  console.log(`        with delta: ${numeral(accelerationDelta).format('0.000')} acc per day, days: ${dayCount}`)
 
-  let weekLater, dateObj, newTotal, daily
+  let projectedDates = last7days
+
+  let totals = {}
+  let velocities = {}
+  let accelerations = {}
+  projectedDates.forEach(d => {
+    totals[d] = entry.totals.deaths[d]
+    velocities[d] = entry.velocity.deaths[d]
+    accelerations[d] = entry.acceleration.deaths[d]
+  })
+
   let table = []
-  last7days.forEach((date, index) => {
+
+  let weekLater, date, dateObj, newTotal, daily
+  for (let index = 0; index < dayCount; index++) {
+    date = projectedDates[index]
+
     dateObj = DateTime.fromISO(date)
     dateObj = dateObj.plus({days: 7})
-    weekLater = `${dateObj.month + 1}/${dateObj.day}`
+    weekLater = dateObj.toISODate()
+    projectedDates = projectedDates.concat(weekLater)
 
     acceleration = acceleration + accelerationDelta
     velocity = velocity + acceleration
 
-    newTotal = entry.totals.deaths[date] + Math.pow(10, velocity)
+    newTotal = Math.max(total, totals[date] + Math.pow(10, velocity))
+    totals[weekLater] = newTotal
     daily = newTotal - total
     total = newTotal
 
     table.push({
       date: weekLater,
+      dateObj,
       total,
       daily,
       v: velocity,
       acc: acceleration,
       explain: `(on ${date}: ${numeral(entry.totals.deaths[date]).format('0,000')} * 10^${numeral(velocity).format('0.000')} = ${numeral(total).format('0,000')})`
     })
+  }
+  last7days.forEach((date, index) => {
+
   })
 
   table.forEach(row => {
     console.log(
-      `%c${padStart(row.date, 10)}:  ${padStart(numeral(row.total).format('0,000'), 6)}`
-      + ` ${padStart('+' + numeral(row.daily).format('0,000'), 6)}`
+      `%c${padStart(`${row.dateObj.month}/${row.dateObj.day}`, 10)}:  ${padStart(numeral(row.total).format('0,000'), 6)}`
+      + ` ${padStart((row.daily > 0 ? '+' : '' ) + numeral(row.daily).format('0,000'), 6)}`
       + `    v: ${padStart(numeral(row.v).format('0.000'), 7)}`
       + `    acc: ${padStart(numeral(row.acc).format('0.000'), 6)}`,
       'color: #777'
